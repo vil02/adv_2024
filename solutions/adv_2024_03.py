dfb9314b84472852fd620b08cb656198bc3db14e
vil02 <65706193+vil02@users.noreply.github.com>
2024-12-03 19:56:44 +0100

    style: simplify `adv_2024_03`

--- solutions/adv_2024_03.py
+++ solutions/adv_2024_03.py
@@ -1,5 +1,4 @@
 import re
-import typing
 
 
 def _evaluate(in_str: str) -> int:
@@ -11,54 +10,21 @@ def _evaluate(in_str: str) -> int:
     return int(str_a) * int(str_b)
 
 
-def _sum_prods(in_str: str, is_enabled: typing.Callable[[int], bool]) -> int:
-    return sum(
-        _evaluate(_.group())
-        for _ in re.finditer(r"mul\(\d{1,3},\d{1,3}\)", in_str)
-        if is_enabled(_.start())
-    )
+_PATTERN = r"mul\(\d{1,3},\d{1,3}\)"
 
 
 def solve_a(in_str: str) -> int:
-    return _sum_prods(in_str, lambda _: True)
-
-
-def _find_all_starts(in_str: str, in_pattern: str) -> list[int]:
-    return [_.start() for _ in re.finditer(in_pattern, in_str)]
-
-
-def _pop_from_starts(
-    cur_pos: int, cur_value: bool, starts: list[int], new_value: bool
-) -> bool:
-    if starts:
-        assert cur_pos <= starts[0]
-        if cur_pos == starts[0]:
-            starts.pop(0)
-            return new_value
-    return cur_value
-
-
-def _consume_starts(
-    in_output_len: int, enabled_starts: list[int], disabled_starts: list[int]
-) -> list[bool]:
-    assert not set(enabled_starts) & set(disabled_starts)
-    res = []
-    is_enabled = True
-    for _ in range(in_output_len):
-        is_enabled = _pop_from_starts(_, is_enabled, enabled_starts, True)
-        is_enabled = _pop_from_starts(_, is_enabled, disabled_starts, False)
-        res.append(is_enabled)
-    return res
-
-
-def _compute_is_enabled(in_str: str) -> list[bool]:
-    return _consume_starts(
-        len(in_str),
-        _find_all_starts(in_str, r"do\(\)"),
-        _find_all_starts(in_str, r"don't\(\)"),
-    )
+    return sum(_evaluate(_.group()) for _ in re.finditer(_PATTERN, in_str))
 
 
 def solve_b(in_str: str) -> int:
-    is_enabled_list = _compute_is_enabled(in_str)
-    return _sum_prods(in_str, lambda pos: is_enabled_list[pos])
+    res = 0
+    is_enabled = True
+    for _ in re.finditer(_PATTERN + r"|do\(\)|don\'t\(\)", in_str):
+        if _.group() == "do()":
+            is_enabled = True
+        elif _.group() == "don't()":
+            is_enabled = False
+        elif is_enabled:
+            res += _evaluate(_.group())
+    return res
