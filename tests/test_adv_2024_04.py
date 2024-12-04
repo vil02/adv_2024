dc78035c8de3dec1d70b31f7a7aa40e617989a8c
vil02 <65706193+vil02@users.noreply.github.com>
2024-12-04 07:10:31 +0100

    feat: add `adv_2024_04`

---
+++ tests/test_adv_2024_04.py
@@ -0,0 +1,10 @@
+import solutions.adv_2024_04 as sol
+from . import test_utils as tu
+
+
+_INPUTS = tu.get_inputs(4, {"small", "p"})
+
+
+test_solve_a, test_solve_b = _INPUTS.get_tests(
+    (sol.solve_a, sol.solve_b), {"small": (18, 9), "p": (2468, 1864)}
+)
