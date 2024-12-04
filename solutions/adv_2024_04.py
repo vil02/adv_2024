2d2ae0e908ea23078fcc4f162e60ef82a5c19c0a
vil02 <65706193+vil02@users.noreply.github.com>
2024-12-04 18:13:55 +0100

    feat: simplify `adv_2024_04.solve_b`

--- solutions/adv_2024_04.py
+++ solutions/adv_2024_04.py
@@ -46,20 +46,16 @@ def solve_a(in_str: str) -> int:
     return sum(_count_at_pos(words, _, "XMAS") for _ in words)
 
 
-def _extract(words: Words, pos: Pos, search_dir: Pos, size: int) -> str:
-    assert size >= 0
-    if size == 0:
-        return ""
-    return words.get(pos, "") + _extract(
-        words, _shift(pos, search_dir), search_dir, size - 1
-    )
-
-
 def _is_x_mas(words: Words, pos: Pos) -> bool:
-    mas = "MAS"
-    diag_0 = _extract(words, _shift(pos, _NW), _SE, len(mas))
-    diag_1 = _extract(words, _shift(pos, _SW), _NE, len(mas))
-    return {diag_0, diag_1} <= {mas, mas[::-1]}
+    def _check_diagonal(pos: Pos, shift: Pos) -> bool:
+        mas = "MAS"
+        return _is_at_pos_in_dir(words, pos, shift, mas) or _is_at_pos_in_dir(
+            words, pos, shift, mas[::-1]
+        )
+
+    return _check_diagonal(_shift(pos, _NW), _SE) and _check_diagonal(
+        _shift(pos, _SW), _NE
+    )
 
 
 def solve_b(in_str: str) -> int:
