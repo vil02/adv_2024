e416588c4d83b5f3ef0d4b25b2a064ad24824e32
vil02 <65706193+vil02@users.noreply.github.com>
2024-12-07 21:12:59 +0100

    refactor: reduce code duplication in `adv_2024_07`

--- solutions/adv_2024_07.py
+++ solutions/adv_2024_07.py
@@ -1,30 +1,34 @@
 import typing
 
 
-class _Equation(typing.NamedTuple):
+class Equation(typing.NamedTuple):
     result: int
     numbers: list[int]
 
 
-def _parse_equation(in_line: str) -> _Equation:
+def _parse_equation(in_line: str) -> Equation:
     result, numbers = in_line.split(": ")
-    return _Equation(int(result), [int(_) for _ in numbers.split(" ")])
+    return Equation(int(result), [int(_) for _ in numbers.split()])
 
 
-def _parse_input(in_str: str) -> list[_Equation]:
+def _parse_input(in_str: str) -> list[Equation]:
     return [_parse_equation(_) for _ in in_str.splitlines()]
 
 
-def _simplify_eq_add(equation: _Equation) -> _Equation:
-    return _Equation(equation.result - equation.numbers[-1], equation.numbers[:-1])
+def _can_be_simplified_add(_: Equation) -> bool:
+    return True
 
 
-def _can_be_simplified_multip(equation: _Equation) -> bool:
+def _simplify_eq_add(equation: Equation) -> Equation:
+    return Equation(equation.result - equation.numbers[-1], equation.numbers[:-1])
+
+
+def _can_be_simplified_multip(equation: Equation) -> bool:
     return equation.result % equation.numbers[-1] == 0
 
 
-def _simplify_eq_multip(equation: _Equation) -> _Equation:
-    return _Equation(equation.result // equation.numbers[-1], equation.numbers[:-1])
+def _simplify_eq_multip(equation: Equation) -> Equation:
+    return Equation(equation.result // equation.numbers[-1], equation.numbers[:-1])
 
 
 def _can_be_converted_to_int(in_str: str) -> bool:
@@ -36,43 +40,49 @@ def _can_be_converted_to_int(in_str: str) -> bool:
     return res
 
 
-def _can_be_simplified_concat(equation: _Equation) -> bool:
+def _can_be_simplified_concat(equation: Equation) -> bool:
     str_res = str(equation.result)
     last_num_str = str(equation.numbers[-1])
     new_res_str = str_res.removesuffix(last_num_str)
     return str_res.endswith(last_num_str) and _can_be_converted_to_int(new_res_str)
 
 
-def _simplify_eq_concat(equation: _Equation) -> _Equation:
+def _simplify_eq_concat(equation: Equation) -> Equation:
     new_res = str(equation.result).removesuffix(str(equation.numbers[-1]))
-    return _Equation(int(new_res), equation.numbers[:-1])
+    return Equation(int(new_res), equation.numbers[:-1])
 
 
-def _can_be_true_a(equation: _Equation) -> bool:
-    assert len(equation.numbers) > 0
-    if len(equation.numbers) == 1:
-        return equation.result == equation.numbers[0]
-    res = _can_be_true_a(_simplify_eq_add(equation))
-    if not res and _can_be_simplified_multip(equation):
-        res |= _can_be_true_a(_simplify_eq_multip(equation))
-    return res
+class _Simplification(typing.NamedTuple):
+    is_applicable: typing.Callable[[Equation], bool]
+    simplify: typing.Callable[[Equation], Equation]
 
 
-def _can_be_true_b(equation: _Equation) -> bool:
-    assert len(equation.numbers) > 0
-    if len(equation.numbers) == 1:
-        return equation.result == equation.numbers[0]
-    res = _can_be_true_b(_simplify_eq_add(equation))
-    if not res and _can_be_simplified_multip(equation):
-        res |= _can_be_true_b(_simplify_eq_multip(equation))
-    if not res and _can_be_simplified_concat(equation):
-        res |= _can_be_true_b(_simplify_eq_concat(equation))
+_Plus = _Simplification(_can_be_simplified_add, _simplify_eq_add)
+_Multip = _Simplification(_can_be_simplified_multip, _simplify_eq_multip)
+_Concat = _Simplification(_can_be_simplified_concat, _simplify_eq_concat)
 
-    return res
+
+def _get_can_be_true(
+    in_simplifications: list[_Simplification],
+) -> typing.Callable[[Equation], bool]:
+    def _can_be_true(equation: Equation) -> bool:
+        assert len(equation.numbers) > 0
+        if len(equation.numbers) == 1:
+            return equation.result == equation.numbers[0]
+        return any(
+            _.is_applicable(equation) and _can_be_true(_.simplify(equation))
+            for _ in in_simplifications
+        )
+
+    return _can_be_true
+
+
+can_be_true_a = _get_can_be_true([_Plus, _Multip])
+can_be_true_b = _get_can_be_true([_Plus, _Multip, _Concat])
 
 
 def _get_solve(
-    can_be_true: typing.Callable[[_Equation], bool]
+    can_be_true: typing.Callable[[Equation], bool]
 ) -> typing.Callable[[str], int]:
     def _solve(in_str: str) -> int:
         return sum(_.result for _ in _parse_input(in_str) if can_be_true(_))
@@ -80,5 +90,5 @@ def _get_solve(
     return _solve
 
 
-solve_a = _get_solve(_can_be_true_a)
-solve_b = _get_solve(_can_be_true_b)
+solve_a = _get_solve(can_be_true_a)
+solve_b = _get_solve(can_be_true_b)
