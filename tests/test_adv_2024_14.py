175bb29208b33bd07f84a5a18e2cfce51451cca9
vil02 <65706193+vil02@users.noreply.github.com>
2024-12-14 19:50:32 +0100

    style: use `get_tests` in `test_adv_2024_14`

--- tests/test_adv_2024_14.py
+++ tests/test_adv_2024_14.py
@@ -4,5 +4,6 @@ from . import test_utils as tu
 
 _INPUTS = tu.get_inputs(14, {"p"})
 
-test_solve_a = _INPUTS.get_test(sol.solve_a, {"p": 224438715})
-test_solve_b = _INPUTS.get_test(sol.solve_b, {"p": 7603})
+test_solve_a, test_solve_b = _INPUTS.get_tests(
+    (sol.solve_a, sol.solve_b), {"p": (224438715, 7603)}
+)
